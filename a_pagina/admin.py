7367eea63eba79be46d437216b67de3f6474d7a2
Jonathan Didier Gimenez Gonzalez <jdg24g@gmail.com>
2025-02-25 23:55:47 -0300

    Enhance Ticket admin interface with inline ItemPedido and custom display

--- a_pagina/admin.py
+++ a_pagina/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
 from .models import Producto, Cliente, Ticket, ItemPedido
 
-# Register your models here.
+class ItemPedidoInline(admin.TabularInline):  # También puedes usar admin.StackedInline
+    model = ItemPedido
+    extra = 0  # Por defecto no mostrar campos extra
+    
+    def get_extra(self, request, obj=None, **kwargs):
+        # Si obj es None, estamos creando un nuevo Ticket
+        # Si obj existe pero no tiene items, también mostramos un campo extra
+        if obj is None or not obj.itempedido_set.exists():
+            return 1
+        return 0  # En otros casos, no mostrar campos extra
+
+
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    inlines = [ItemPedidoInline]
+    list_display = ['id', 'cliente', 'tipo', 'fecha_creacion']
+
 admin.site.register(Producto)
 admin.site.register(Cliente)
-admin.site.register(Ticket)
 admin.site.register(ItemPedido)
-

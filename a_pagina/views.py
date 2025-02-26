c24ba5afc5fabe4e87be2fd21815ddc7afa0517e
Jonathan Didier Gimenez Gonzalez <jdg24g@gmail.com>
2025-02-25 23:18:32 -0300

    fix_migrate

--- a_pagina/views.py
+++ a_pagina/views.py
@@ -5,3 +5,11 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hola mundo")
+
+def imprimir_ticket(request):
+    return HttpResponse("Imprimir ticket")
+
+def vista_previa_ticket(request):
+    return HttpResponse("Vista previa ticket")
+
+
